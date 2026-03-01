3dba9e78b455407d0565e35e30a06420667b4586
architi <aryaamightbeclueless@gmail.com>
2026-03-01 22:57:02 +0530

    refactor: wrapped home inside a protected layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-
+import HomePage from "./pages/home.jsx";
+import Protected from "./components/protected.jsx";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<Protected />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<h1>Hello</h1>} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
+
+
+
