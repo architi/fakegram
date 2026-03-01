ac6efe83beddad9bfedab2b3ef34a2e16e2f778e
architi <aryaamightbeclueless@gmail.com>
2026-03-01 22:54:48 +0530

    feat:signOut functionality and added tryCatch

--- src/firebaseApi/auth.js
+++ src/firebaseApi/auth.js
@@ -1,34 +1,32 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, getAuth,signOut} from "firebase/auth";
+import { app } from "./firebase.js";
 
-
-// createUserWithEmailAndPassword(auth, email, password)
-//   .then((userCredential) => {
-//     // Signed up
-//     const user = userCredential.user;
-//     // ...
-//   })
-//   .catch((error) => {
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // ..
-//   });
+export const auth = getAuth(app);
 
 export const authApi = {
   async signUp(email, password) {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
-    localStorage.setItem("token",user.accessToken);
+    try {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+    } catch (error) {
+        const errorCode =error.code;
+        const errorMessage = error.message;
+        console.log(errorCode,errorMessage);
+    }
   },
+
   async signIn(email, password) {
-    const userCreds = await signInWithEmailAndPassword(auth, email, password);
-    const {user} = userCreds;
-    localStorage.setItem("token",user.accessToken);
+    try {
+        const userCreds = await signInWithEmailAndPassword(auth, email, password);
+        const {user} = userCreds;
+    } catch (error) {
+        console.log(error.code, error.message);
+    }
   },
-   getUser(){
-    const user = auth.currentUser;
-    console.log(user);
-    return user;
+
+  async signOut(){
+    await signOut(auth);
   }
 };
-// authApi.signUp("test@example.com", "password123");
+
+
